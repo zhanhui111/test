a3b2e0a5266612b7eaab594d8e3064cee6b60377
zhanhui111 <75153011+zhanhui111@users.noreply.github.com>
2025-03-15 20:57:33 +0800

    Update decode_weights.m

--- decode_weights.m
+++ decode_weights.m
@@ -1,8 +1,1 @@
-function [w1, B1, w2, B2] = decode_weights(position, inputnum, hiddennum, outputnum)
-    % 权重解码函数
-    w1 = position(1 : inputnum * hiddennum);
-    B1 = position(inputnum * hiddennum + 1 : inputnum * hiddennum + hiddennum);
-    w2 = position(inputnum * hiddennum + hiddennum + 1 : ...
-                 inputnum * hiddennum + hiddennum + hiddennum * outputnum);
-    B2 = position(end - outputnum + 1 : end);
-end
+
